e3756e8247fba5d6f65a53c2156133704ad4f490
shivaniverma1606 <45076866+shivaniverma1998@users.noreply.github.com>
2024-04-10 13:56:32 +0000

    datatypes in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const employeeId = 1234;
+let accountEmail = "shivaniverma1606p@gmail.com";
+var accountPassword = "12345";
+accountCity = "Noida";
+let accountState;
+
+accountCity = "Faridabad"
+accountState = "Hyderabad";
+
+console.table([employeeId, accountEmail, accountPassword, accountCity, accountState])
