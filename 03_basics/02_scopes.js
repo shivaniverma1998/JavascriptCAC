527a2cb1c6185a84820ed32b5fe546a325a1299d
shivaniverma1606 <45076866+shivaniverma1998@users.noreply.github.com>
2024-04-22 18:29:22 +0000

    functions scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -19,6 +19,41 @@ if (true) {
 console.log(a);
 //console.log(b);
 //console.log(c);
+//functions and their scope 
 
+function one(){
+    const username = "shivani"
 
+    function two(){
+        const gender = "female"
+        console.log(username);  //yeh child function  parent k variable  access kr skta h but parent ni kr skta(parent : one)
+    }
+    //console.log(gender);
+    two()
+}
+one()
+
+if(true){
+
+    const username = "shivani"
+    if(username ==="shivani"){
+        const website = " meta"
+        console.log(username + website)
+    }
+   // console.log(website);  ispe error ayega kyuki website variable  scope k andar h access ni kr skte
+}
+//console.log(username);  isme bhi error ayega bcz username is in the scope and we are trying to acces outside the scope
+
+//+++++++++++++++++++++++++ iteresting ++++++++++++++4
+
+function addone(num){
+    return num +1
+}
+
+addone(5)
+
+addtwo(5)   /// yhaa pr ek interesting concept  ki hamne phle declaration se phle access krna chaha  th iisliye error aya kyuki function variable k thrugh declaare hua  
+const addtwo = function(num){  // but hamne yhaa par function ko  value me daaala hai 
+    return num + 2
+}
 
