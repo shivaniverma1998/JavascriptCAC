804eff1b2aaec79d3c4f65717ec710bb26093f54
shivaniverma1606 <45076866+shivaniverma1998@users.noreply.github.com>
2024-04-25 08:09:04 +0000

    call stack and function

---
+++ 03_basics/05_callstack.js
@@ -0,0 +1,15 @@
+//How does javascript execute code + call stack
+
+//Concept - Javascript Execution Context - how javascript  run a file 
+there are two phases in which files executes 1. Global EC  {}   for example  -->this(iski value window object)
+2. Functional Execution Context
+
+{} this is memory creation phase , (2) execution phase
+
+
+lets take piece of code to understand how javascript code Worker
+
+//refer image.png : phase (1)  global esecution(this)---> phase(2) memory phase  value define (e.g val1 -> undefined) --->  phase(3) -> Execution phase (e.g val1 <- 10)
+ 
+
+//CALL STACK  : REFER image (callstack.png & callstackfn.png)
