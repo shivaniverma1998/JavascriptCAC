5c03161d7a2a187540311820acd3299df9f49bf6
shivaniverma1606 <45076866+shivaniverma1998@users.noreply.github.com>
2024-06-28 09:45:09 +0000

    analog clock

---
+++ 20DaysJS-suneja/helperfunction.js
@@ -0,0 +1,33 @@
+// A helper recursive function in JavaScript is a function that is used to help another recursive function. It is often used to simplify the code of the main recursive function or to provide additional functionality.
+// Helper recursive functions are typically called by the main recursive function and then they call themselves recursively. This process continues until the base case is reached, at which point the helper recursive function returns a value to the main recursive function.
+// Here is an example of a helper recursive function in JavaScript:
+
+// bahar waale ffn ko recursivvely call na krwake andar waale helper fn ko recursively call krwate rehna hi helper fn ka kaam hai
+
+// function factorial(n) {
+//     if (n === 0) {
+//       return 1;
+//     } else {
+//       return n * factorial(n - 1);
+//     }
+//   }
+  
+//   function helper(n) {
+//     if (n === 0) {
+//       return 1;
+//     } else {
+//       return n * helper(n - 1);
+//     }
+//   }
+  
+//   const res = factorial(5); // returns 120
+//   console.log(res);
+//   const pes = helper(5); // returns 120
+//   console.log(pes);
+
+let myarray = [1,2,3,4,5]
+let i = 0;
+function fndeven(array){
+    
+    
+}
